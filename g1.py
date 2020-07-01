99a0d3e8af732209eb6fdfaf4b21f38227a2ecb3
Hzaakk <43729496+Hzaakk@users.noreply.github.com>
2020-07-01 04:12:07 +0300

    Add files via upload

---
+++ g1.py
@@ -0,0 +1,28 @@
+I=eval(input().replace('][','],['))
+R,C = len(I),len(I[0])
+f=range
+def t():
+	for r in f(R):
+		for c in f(C):
+			y=i[r][c]
+			x=y>>2
+			l=i[r][c-1]if c!=0else-1
+			d=i[r+1][c]if r!=R-1else-1
+			if r==0and y&1or r==R-1and y&4or c==0and y&8or c==C-1and y&2or l!=-1and(x^l)&2!=0 or d!=-1and (x^d)&1!=0:
+				return 0
+	return 1
+for x in f(4**(R*C)):
+	i=I.copy()
+	b=[]
+	while x>0:
+		b.insert(0,x%4)
+		x//=4
+	for j in f(len(b)):
+		y,x=j//C,j%C
+		n=i[y][x]
+		i[y][x]=(n>>b[j])|(n<<4-b[j])&15
+	if t():
+		print(*i,sep='')
+		break
+else:
+	print(-1)
